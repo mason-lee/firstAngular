b37afd027560fb239771d8f31602da7aab26762f
Mason Lee <mason.mideumlee@gmail.com>
2014-08-26 13:44:17 -0700

    Fix the footer

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -11,7 +11,7 @@
 
 // Create a module and name it myApp
 	// Also include ngRoute for all our routing needs
-var myApp = angular.module('myApp', ['ngRoute', 'ngAnimate', 'bzSlider']);
+var myApp = angular.module('myApp', ['ngRoute', 'ngAnimate', 'bzSlider', 'snap']);
 
 // Configure our routes
 myApp.config(function($routeProvider, $locationProvider) {
@@ -19,32 +19,42 @@ myApp.config(function($routeProvider, $locationProvider) {
 		// route for home page
 		.when('/', {
 			templateUrl: 'views/main.html',
-			controller: 'mainController'
+			controller: 'mainController',
+			activetab: 'home'
 		})
 
 		// route for about page
 		.when('/about', {
 			templateUrl: 'views/about.html',
-			controller: 'aboutController'
+			controller: 'aboutController',
+			activetab: 'about'
 		})
 
 		//route for project page
 		.when('/project', {
 			templateUrl: 'views/project.html',
-			controller: 'projectController'
+			controller: 'projectController',
+			activetab: 'project'
 		})
 
 		// route for contact page
 		.when('/contact', {
 			templateUrl: 'views/contact.html',
-			controller: 'contactController'
-		});
+			controller: 'contactController',
+			activetab: 'contact'
+		})
+
+		.otherwise({ redirectTo: '/' });
 
 	// Use the HTML5 History API
 	if(window.history && window.history.pushState){
 		$locationProvider.html5Mode(true);
 	}
-});
+}).run(['$rootScope', '$http', '$browser', '$timeout', '$route', function($scope, $http, $browser, $timeout, $route) {
+	$scope.$on('$routeChangeSuccess', function(scope, next, current) {
+		$scope.part = $route.current.activetab;
+	});
+}]);
 
 myApp.controller('mainController', function($scope) {
 	$scope.delay = 2000;
@@ -56,13 +66,13 @@ myApp.controller('mainController', function($scope) {
 });
 
 myApp.controller('aboutController', function($scope) {
-	$scope.message = 'I am at about page.';
+	
 });
 
 myApp.controller('projectController', function($scope) {
-	$scope.message = 'I am at project page.';
+	
 });
 
 myApp.controller('contactController', function($scope) {
-	$scope.message = 'I am at contact page';
+	
 });
